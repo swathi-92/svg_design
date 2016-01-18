fe0597228dc71f5b75b649e21a4a5d1223b85580
unknown <spatlolla@IM-RT-IT-160.innomindshyd.com>
2016-01-18 11:27:57 +0530

    https://plnkr.co/edit/iPKWsLuxtY2aJ0CgiNRO?p=preview

---
+++ JS/mainjs.js
@@ -0,0 +1,7 @@
+function svgFunction() {
+   var a= document.getElementById("xAxis").value;
+   var b=document.getElementById("yAxis").value;
+   var c=document.getElementsByTagName("circle");
+   var d=c[0].setAttribute('cx',a);
+   var e=c[0].setAttribute('cy',b);
+}
